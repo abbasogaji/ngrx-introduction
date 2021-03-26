1e9e457b7b19e879a0c04d22cc6b3db3195831e3
Abbas Ogaji <abbasogaji@gmail.com>
2021-03-26 23:49:05 +0100

    chore: updated styles and imports

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,22 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './pages/home/home.component';
+import { TeamBlueComponent } from './pages/team-blue/team-blue.component';
+import { TeamGreenComponent } from './pages/team-green/team-green.component';
+import { UsersComponent } from './pages/users/users.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path : "", component : HomeComponent},
+  {path : "users", component : UsersComponent},
+  {path : "team-blue-users", component : TeamBlueComponent },
+  {path : "team-green-users", component : TeamGreenComponent }
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [
+    RouterModule.forRoot(routes)
+  
+  ],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,27 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './pages/home/home.component';
+import { UsersComponent } from './pages/users/users.component';
+import { FormsModule } from '@angular/forms';
+import { StoreModule } from '@ngrx/store';
+import { userReducer } from './store/user/user.reducers';
+import { TeamGreenComponent } from './pages/team-green/team-green.component';
+import { TeamBlueComponent } from './pages/team-blue/team-blue.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    UsersComponent,
+    TeamGreenComponent,
+    TeamBlueComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    FormsModule,
+    AppRoutingModule,
+    StoreModule.forRoot({ user : userReducer})
   ],
   providers: [],
   bootstrap: [AppComponent]
